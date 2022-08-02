6f18014dc8ad9de6fb2d1da2c0ba99c30ddd090f
tekimanvi <107298271+tekimanvi@users.noreply.github.com>
2022-08-02 17:10:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -20,4 +20,24 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 
+let myFlashcards = [ {
+    "question": "Name this species of bird.",
+    "image" : "images/barn-owl-2550068__340.webp",
+    "answer": "Barn Owl"
+    },
+    {
+    "question": "Name this species of bird.",
+    "image" : "images/dove-2516641__340.webp",
+    "answer": "Dove"
+    },
+    {
+    "question": "Name this species of bird.",
+    "image" : "images/hummingbird-2139279__340.webp",
+    "answer": "Hummingbird"
+    }
+  ];
+
+
+let dbLocation = firebase.database().ref('deck/flashcards'); 
+dbLocation.set(myFlashcards);
 
