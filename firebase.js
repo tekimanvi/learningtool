2a1fc092a3ca1a039d8f1d5993501822ac9d0447
tekimanvi <107298271+tekimanvi@users.noreply.github.com>
2022-08-19 19:57:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
